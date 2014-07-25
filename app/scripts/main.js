4986481e60de506043caee0599445122103604c7
James Shakespeare <j@jshakespeare.com>
2014-07-25 13:47:29 +0100

    yo scaffold

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Clock, $*/
+
+
+window.Clock = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Clock.init();
+});
